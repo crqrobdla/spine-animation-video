6dc7ec73d34ca469a0cfb91bca7b3dd8739abed3
crqrobdla <s25880260@gmail.com>
2024-11-06 11:39:53 +0800

    Update videoData.js

--- videoData.js
+++ videoData.js
@@ -60,331 +60,331 @@ const videoData = {
         ],
         skeleton: [{
                 title: "Idle",
-                url: "./video/skeleton/Idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/skeleton/Idle.mov"
             }, {
                 title: "Attack",
-                url: "./video/skeleton/Attack.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/skeleton/Attack.mov"
             }
         ],
         PROMETHEUS: [{
                 title: "idle",
-                url: "./video/PROMETHEUS/idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/PROMETHEUS/idle.mov"
             }, {
                 title: "attack",
-                url: "./video/PROMETHEUS/attack.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/PROMETHEUS/attack.mov"
             }
         ],
         ChiYou: [{
                 title: "idle",
-                url: "./video/ChiYou/idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/ChiYou/idle.mov"
             }, {
                 title: "attack",
-                url: "./video/ChiYou/attack.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/ChiYou/attack.mov"
             }
         ],
         poon: [{
                 title: "idle",
-                url: "./video/poon/idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/poon/idle.mov"
             }, {
                 title: "attack",
-                url: "./video/poon/attack.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/poon/attack.mov"
             }
         ],
         QingGui: [{
                 title: "Idle",
-                url: "./video/QingGui/Idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/QingGui/Idle.mov"
             }, {
                 title: "Attack",
-                url: "./video/QingGui/Attack.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/QingGui/Attack.mov"
             }
         ],
         Montana: [{
                 title: "idle",
-                url: "./video/Montana/idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/Montana/idle.mov"
             }, {
                 title: "attack",
-                url: "./video/Montana/attack_crop.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/Montana/attack_crop.mov"
             }
         ],
         Scáthach: [{
                 title: "Idle",
-                url: "./video/Scáthach/Idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/Scáthach/Idle.mov"
             }, {
                 title: "Attack",
-                url: "./video/Scáthach/Attack.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/Scáthach/Attack.mov"
             }
         ],
         HJJ: [{
                 title: "idle",
-                url: "./video/HJJ/idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/HJJ/idle.mov"
             }, {
                 title: "attack",
-                url: "./video/HJJ/attack.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/HJJ/attack.mov"
             }
         ],
         pandora: [{
                 title: "idle",
-                url: "./video/pandora/idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/pandora/idle.mov"
             }, {
                 title: "attack",
-                url: "./video/pandora/attack.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/pandora/attack.mov"
             }
         ],
         sodom: [{
                 title: "idle",
-                url: "./video/sodom/idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/sodom/idle.mov"
             }, {
                 title: "attack",
-                url: "./video/sodom/attack_crop.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/sodom/attack_crop.mov"
             }
         ],
         Luker: [{
                 title: "idle",
-                url: "./video/Luker/idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/Luker/idle.mov"
             }, {
                 title: "attack",
-                url: "./video/Luker/attack_crop.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/Luker/attack_crop.mov"
             }
         ],
         Vatican1: [{
                 title: "idle",
-                url: "./video/Vatican1/idle_crop.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/Vatican1/idle_crop.mov"
             }, {
                 title: "attack",
-                url: "./video/Vatican1/attack.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/Vatican1/attack.mov"
             }
         ],
         lindaiyu: [{
                 title: "idle",
-                url: "./video/lindaiyu/idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/lindaiyu/idle.mov"
             }, {
                 title: "attack",
-                url: "./video/lindaiyu/attack_crop.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/lindaiyu/attack_crop.mov"
             }
         ],
         nero: [{
                 title: "idle",
-                url: "./video/nero/idle.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/nero/idle.mov"
             }, {
                 title: "attack",
-                url: "./video/nero/attack_crop.mov"
+                url: "https://crqrobdla.github.io/spine-animation-video/video/nero/attack_crop.mov"
             }
         ]
     },
     baseroomcharacter: {
         categories: [{
                 category: "Shroy_sep",
-                src: "./img/SecretBaseCharacter_1_rgba.png",
+                src: "https://crqrobdla.github.io/spine-animation-video/img/SecretBaseCharacter_1_rgba.png",
                 alt: "Shroy_sep"
             }, {
                 category: "OureaV2_test",
-                src: "./img/SecretBaseCharacter_2_rgba.png",
+                src: "https://crqrobdla.github.io/spine-animation-video/img/SecretBaseCharacter_2_rgba.png",
                 alt: "OureaV2_test"
             }, {
                 category: "Melody0802merge",
-                src: "./img/SecretBaseCharacter_3_rgba.png",
+                src: "https://crqrobdla.github.io/spine-animation-video/img/SecretBaseCharacter_3_rgba.png",
                 alt: "Melody0802merge"
             }, {
                 category: "Snow_0727",
-                src: "./img/SecretBaseCharacter_4_rgba.png",
+                src: "https://crqrobdla.github.io/spine-animation-video/img/SecretBaseCharacter_4_rgba.png",
                 alt: "Snow_0727"
             }, {
                 category: "BlackGaGa_edit",
-                src: "./img/SecretBaseCharacter_5_rgba.png",
+                src: "https://crqrobdla.github.io/spine-animation-video/img/SecretBaseCharacter_5_rgba.png",
                 alt: "BlackGaGa_edit"
             }, {
                 category: "Nocturne0318B",
-                src: "./img/SecretBaseCharacter_6_rgba.png",
+                src: "https://crqrobdla.github.io/spine-animation-video/img/SecretBaseCharacter_6_rgba.png",
                 alt: "Nocturne0318B"
             }, {
                 category: "LIN_edit",
-                src: "./img/SecretBaseCharacter_7_rgba.png",
+                src: "https://crqrobdla.github.io/spine-animation-video/img/SecretBaseCharacter_7_rgba.png",
                 alt: "LIN_edit"
             }, {
                 category: "kamila_edit",
-                src: "./img/SecretBaseCharacter_8_rgba.png",
+                src: "https://crqrobdla.github.io/spine-animation-video/img/SecretBaseCharacter_8_rgba.png",
                 alt: "kamila_edit"
             }
         ],
         Shroy_sep: [{
                 title: "Idle1",
-                url: "./gif/Shroy_sep/Idle1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Shroy_sep/Idle1.gif"
             }, {
                 title: "Idle2",
-                url: "./gif/Shroy_sep/Idle2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Shroy_sep/Idle2.gif"
             }, {
                 title: "PickUp",
-                url: "./gif/Shroy_sep/PickUp.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Shroy_sep/PickUp.gif"
             }, {
                 title: "Reaction",
-                url: "./gif/Shroy_sep/Reaction.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Shroy_sep/Reaction.gif"
             }, {
                 title: "Touch1",
-                url: "./gif/Shroy_sep/Touch1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Shroy_sep/Touch1.gif"
             }, {
                 title: "Touch2",
-                url: "./gif/Shroy_sep/Touch2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Shroy_sep/Touch2.gif"
             }, {
                 title: "Walk",
-                url: "./gif/Shroy_sep/Walk.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Shroy_sep/Walk.gif"
             }
         ],
         OureaV2_test: [{
                 title: "Idle1",
-                url: "./gif/OureaV2_test/Idle1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/OureaV2_test/Idle1.gif"
             }, {
                 title: "Idle2",
-                url: "./gif/OureaV2_test/Idle2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/OureaV2_test/Idle2.gif"
             }, {
                 title: "PickUp",
-                url: "./gif/OureaV2_test/PickUp.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/OureaV2_test/PickUp.gif"
             }, {
                 title: "Reaction",
-                url: "./gif/OureaV2_test/Reaction.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/OureaV2_test/Reaction.gif"
             }, {
                 title: "Touch1",
-                url: "./gif/OureaV2_test/Touch1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/OureaV2_test/Touch1.gif"
             }, {
                 title: "Touch2",
-                url: "./gif/OureaV2_test/Touch2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/OureaV2_test/Touch2.gif"
             }, {
                 title: "Unuse_Idle1",
-                url: "./gif/OureaV2_test/Unuse_Idle1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/OureaV2_test/Unuse_Idle1.gif"
             }, {
                 title: "Unuse_Touch1",
-                url: "./gif/OureaV2_test/Unuse_Touch1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/OureaV2_test/Unuse_Touch1.gif"
             }, {
                 title: "Walk",
-                url: "./gif/OureaV2_test/Walk.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/OureaV2_test/Walk.gif"
             }
         ],
         Melody0802merge: [{
                 title: "Idle1",
-                url: "./gif/Melody0802merge/Idle1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Melody0802merge/Idle1.gif"
             }, {
                 title: "Idle2",
-                url: "./gif/Melody0802merge/Idle2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Melody0802merge/Idle2.gif"
             }, {
                 title: "PickUp",
-                url: "./gif/Melody0802merge/PickUp.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Melody0802merge/PickUp.gif"
             }, {
                 title: "Touch1",
-                url: "./gif/Melody0802merge/Touch1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Melody0802merge/Touch1.gif"
             }, {
                 title: "Touch2",
-                url: "./gif/Melody0802merge/Touch2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Melody0802merge/Touch2.gif"
             }, {
                 title: "Walk",
-                url: "./gif/Melody0802merge/Walk.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Melody0802merge/Walk.gif"
             }
         ],
         Snow_0727: [{
                 title: "Idle1",
-                url: "./gif/Snow_0727/Idle1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Snow_0727/Idle1.gif"
             }, {
                 title: "Idle2",
-                url: "./gif/Snow_0727/Idle2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Snow_0727/Idle2.gif"
             }, {
                 title: "PickUp",
-                url: "./gif/Snow_0727/PickUp.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Snow_0727/PickUp.gif"
             }, {
                 title: "Touch1",
-                url: "./gif/Snow_0727/Touch1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Snow_0727/Touch1.gif"
             }, {
                 title: "Touch2",
-                url: "./gif/Snow_0727/Touch2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Snow_0727/Touch2.gif"
             }, {
                 title: "Walk",
-                url: "./gif/Snow_0727/Walk.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Snow_0727/Walk.gif"
             }
         ],
         LIN_edit: [{
                 title: "Idle1",
-                url: "./gif/LIN_edit/Idle1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/LIN_edit/Idle1.gif"
             }, {
                 title: "Idle2",
-                url: "./gif/LIN_edit/Idle2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/LIN_edit/Idle2.gif"
             }, {
                 title: "PickUp",
-                url: "./gif/LIN_edit/PickUp.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/LIN_edit/PickUp.gif"
             }, {
                 title: "Reaction",
-                url: "./gif/LIN_edit/Reaction.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/LIN_edit/Reaction.gif"
             }, {
                 title: "Touch1",
-                url: "./gif/LIN_edit/Touch1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/LIN_edit/Touch1.gif"
             }, {
                 title: "Touch2",
-                url: "./gif/LIN_edit/Touch2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/LIN_edit/Touch2.gif"
             }, {
                 title: "Walk",
-                url: "./gif/LIN_edit/Walk.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/LIN_edit/Walk.gif"
             }
         ],
         Nocturne0318B: [{
                 title: "Idle1",
-                url: "./gif/Nocturne0318B/Idle1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Nocturne0318B/Idle1.gif"
             }, {
                 title: "Idle2",
-                url: "./gif/Nocturne0318B/Idle2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Nocturne0318B/Idle2.gif"
             }, {
                 title: "PickUp",
-                url: "./gif/Nocturne0318B/PickUp.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Nocturne0318B/PickUp.gif"
             }, {
                 title: "Reaction",
-                url: "./gif/Nocturne0318B/Reaction.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Nocturne0318B/Reaction.gif"
             }, {
                 title: "Touch1",
-                url: "./gif/Nocturne0318B/Touch1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Nocturne0318B/Touch1.gif"
             }, {
                 title: "Touch2",
-                url: "./gif/Nocturne0318B/Touch2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Nocturne0318B/Touch2.gif"
             }, {
                 title: "Walk",
-                url: "./gif/Nocturne0318B/Walk.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/Nocturne0318B/Walk.gif"
             }
         ],
         BlackGaGa_edit: [{
                 title: "Idle1",
-                url: "./gif/BlackGaGa_edit/Idle1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/BlackGaGa_edit/Idle1.gif"
             }, {
                 title: "Idle2",
-                url: "./gif/BlackGaGa_edit/Idle2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/BlackGaGa_edit/Idle2.gif"
             }, {
                 title: "PickUp",
-                url: "./gif/BlackGaGa_edit/PickUp.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/BlackGaGa_edit/PickUp.gif"
             }, {
                 title: "Touch1",
-                url: "./gif/BlackGaGa_edit/Touch1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/BlackGaGa_edit/Touch1.gif"
             }, {
                 title: "Touch2",
-                url: "./gif/BlackGaGa_edit/Touch2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/BlackGaGa_edit/Touch2.gif"
             }, {
                 title: "Walk",
-                url: "./gif/BlackGaGa_edit/Walk.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/BlackGaGa_edit/Walk.gif"
             }
         ],
 		kamila_edit: [{
                 title: "Idle1",
-                url: "./gif/kamila_edit/Idle1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/kamila_edit/Idle1.gif"
             }, {
                 title: "Idle2",
-                url: "./gif/kamila_edit/Idle2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/kamila_edit/Idle2.gif"
             }, {
                 title: "PickUp",
-                url: "./gif/kamila_edit/PickUp.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/kamila_edit/PickUp.gif"
             }, {
                 title: "Reaction",
-                url: "./gif/kamila_edit/Reaction.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/kamila_edit/Reaction.gif"
             }, {
                 title: "Touch1",
-                url: "./gif/kamila_edit/Touch1.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/kamila_edit/Touch1.gif"
             }, {
                 title: "Touch2",
-                url: "./gif/kamila_edit/Touch2.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/kamila_edit/Touch2.gif"
             }, {
                 title: "Walk",
-                url: "./gif/kamila_edit/Walk.gif"
+                url: "https://crqrobdla.github.io/spine-animation-video/gif/kamila_edit/Walk.gif"
             }
         ]
     }
